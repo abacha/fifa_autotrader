b32cf1c00ae2a555c4fa9c6b7bd4a9abe3992da1
Adriano Bacha <abacha@gmail.com>
2021-01-20 11:26:23 -0300

    arquivos nao utilizados

--- features/specs/homebroker/pages/base_page.rb
+++ features/specs/homebroker/pages/base_page.rb
@@ -4,13 +4,6 @@ require 'csv'
 class BasePage
   include Capybara::DSL
 
-  attr_reader :elements
-
-  def initialize
-    raw_elements = YAML.load_file('features/specs/homebroker/elements.yml')
-    @elements = OpenStruct.new(raw_elements[self.class.name])
-  end
-
   def page_name
     self.class.name
   end
