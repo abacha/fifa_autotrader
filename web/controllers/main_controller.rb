d13554678432331f8bbec011197c6a9e625bde70
Adriano Bacha <abacha@gmail.com>
2021-02-09 15:50:57 -0300

    spike de gráficos

--- web/controllers/main_controller.rb
+++ web/controllers/main_controller.rb
@@ -14,3 +14,12 @@ get '/log' do
   @log = Rack::Utils.escape_html RobotLogger.tail(300)
   haml :_log
 end
+
+get '/graphs' do
+  @daily_profit = Hash[MatchedTrade.all.group(:date).sum(:profit).map { |k, v| [k.to_s, v] }]
+  @trades  = Hash[MatchedTrade.all.group(:date).count.map { |k, v| [k.to_s, v] }]
+  @avg_profit = Hash[MatchedTrade.all.group(:date).average(:profit).map { |k, v| [k.to_s, v.round] }]
+  @dates = @daily_profit.keys
+  @stock = Stock.all
+  haml :graphs
+end
