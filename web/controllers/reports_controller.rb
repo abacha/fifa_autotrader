0a5231e5d6cc85161eb1b5d55576f959c0eaa084
Adriano Bacha <abacha@gmail.com>
2021-03-16 00:33:49 -0300

    refactoring reports

--- web/controllers/reports_controller.rb
+++ web/controllers/reports_controller.rb
@@ -1,17 +1,14 @@
 # frozen_string_literal: true
 
 class ReportsController < ApplicationController
-  get '/charts' do
-    @q = MatchedTrade.ransack(params[:q])
-    matched_trades = @q.result
+  get '/' do
     grouped = matched_trades.group(:date)
     @dates = grouped.pluck(:date).map(&:to_s)
+    @trades = trades.group(:date, :kind).count.map do |key, value|
+      [key[0].to_s, key[1], value]
+    end
 
     @daily_profit = grouped.sum(:profit).map { |k, v| [k.to_s, v] }
-    @avg_profit = grouped.average(:profit).map { |k, v| [k.to_s, v] }
-
-    @trades = Trade.ransack(params[:q]).result.group(:date, :kind).count.map { |k, v| [k[0].to_s, k[1], v] }
-
     @keys = ((Date.today - 7)..Date.today).to_a.map(&:to_s)
     @players = @keys.map do |date|
       matched_trades.where(date: date).group(:player_name).count
@@ -19,13 +16,28 @@ class ReportsController < ApplicationController
 
     if params[:q]
       @player_report = PlayerReport.new(params[:q][:player_name_eq]).report
+    else
+      @player_report = PlayerReport.all
     end
 
-    haml :'/reports/charts'
+    haml :'/reports/index'
   end
 
-  get '/players' do
-    @reports = PlayerReport.all
-    haml :'/reports/players'
+  private
+
+  def matched_trades
+    @matched_trades ||=
+      begin
+        @q = MatchedTrade.ransack(params[:q])
+        matched_trades = @q.result
+      end
+  end
+
+  def trades
+    @trades ||=
+      begin
+        @q = Trade.ransack(params[:q])
+        @q.result
+      end
   end
 end
