3f872d6fe51bdd13a130c64174eb9f0314ad250e
Adriano Bacha <abacha@gmail.com>
2021-03-16 00:47:47 -0300

    crud de player snipe

---
+++ web/controllers/player_snipes_controller.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+class PlayerSnipesController < ApplicationController
+  before do
+    @players = PlayerSnipe.all.order(status: :desc, name: :asc)
+    params['player'] ||= {}
+  end
+
+  get '/' do
+    @player = PlayerSnipe.find_or_initialize_by(id: params['id'])
+    haml :'player_snipes/index'
+  end
+
+  post '/' do
+    @player = PlayerSnipe.find_or_initialize_by(id: params['player']['id'])
+
+    if @player.update(params['player'])
+      redirect '/player_snipes'
+    else
+      haml :'player_snipes/index'
+    end
+  end
+
+  get '/status' do
+    player = PlayerSnipe.find_by(name: params['name'])
+    player.update!(status: params['status'].to_i)
+    redirect '/player_snipes'
+  end
+end
